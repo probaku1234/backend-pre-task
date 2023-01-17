0183eaf3c252d4438aab83aca487d74aade97667
Yun Seob Lee <fhzotxldj@gmail.com>
2023-01-17 15:09:52 +0900

    add try catch

--- backend/src/routes/profileCard/index.ts
+++ backend/src/routes/profileCard/index.ts
@@ -8,82 +8,119 @@ interface profileCardListOutput extends Record<string, unknown> {
 const router = express.Router()
 
 router.get('/', async (req: Request, res: Response) => {
-  const current = Number(req.query.current as string)
-  const columns = req.query.columns as string[]
-  const pageSize = Number(req.query.pageSize as string)
-  const sort = req.query.sort as string[]
+  try {
+    const current = Number(req.query.current as string)
+    const columns = req.query.columns as string[]
+    const pageSize = Number(req.query.pageSize as string)
+    const sort = req.query.sort as string[]
 
-  const result = await profileCardService.getAll(
-    current - 1,
-    columns,
-    pageSize,
-    sort
-  )
-  const list: profileCardListOutput[] = []
+    const result = await profileCardService.getAll(
+      current - 1,
+      columns,
+      pageSize,
+      sort
+    )
+    const list: profileCardListOutput[] = []
 
-  result.profiles.forEach((value) => {
-    list.push({
-      id: value.id,
-      name: value.name,
-      ...value.information,
-      ...value.careers?.at(0)?.getDataValue('information'),
+    result.profiles.forEach((value) => {
+      list.push({
+        id: value.id,
+        name: value.name,
+        ...value.information,
+        ...value.careers?.at(0)?.getDataValue('information'),
+      })
     })
-  })
 
-  res.send({
-    list,
-    total: result.totalPages,
-  })
+    res.send({
+      list,
+      total: result.totalPages,
+    })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 router.get('/columns', async (req: Request, res: Response) => {
-  const result = await profileCardService.getColumns()
+  try {
+    const result = await profileCardService.getColumns()
 
-  res.send({
-    columns: result,
-  })
+    res.send({
+      columns: result,
+    })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 router.post('/create', async (req: Request, res: Response) => {
-  const result = await profileCardService.createNewProfile(
-    req.body.createTargetName
-  )
+  try {
+    const result = await profileCardService.createNewProfile(
+      req.body.createTargetName
+    )
 
-  res.send({
-    created: true,
-  })
+    if (!result) {
+      res.send({
+        created: false,
+      })
+      return
+    }
+
+    res.send({
+      created: true,
+    })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 router.get('/detail', async (req: Request, res: Response) => {
-  const result = await profileCardService.fetchDetail(Number(req.query.id))
+  try {
+    const result = await profileCardService.fetchDetail(Number(req.query.id))
 
-  res.send({
-    profileCardDetail: {
-      value: result.newResult,
-      valueStructures: result.columnList,
-      name: result.newResult.name,
-    },
-  })
+    res.send({
+      profileCardDetail: {
+        value: result.newResult,
+        valueStructures: result.columnList,
+        name: result.newResult.name,
+      },
+    })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 router.post('/delete', async (req: Request, res: Response) => {
-  await profileCardService.deleteProfile(req.body.profileCardId)
+  try {
+    await profileCardService.deleteProfile(req.body.profileCardId)
 
-  res.sendStatus(200)
+    res.sendStatus(200)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 router.post('/update', async (req: Request, res: Response) => {
-  const result = await profileCardService.updateProfile(
-    req.body.profileCardId,
-    req.body.newValue,
-    req.body.parentDataKey,
-    req.body.itemIndex
-  )
+  try {
+    const result = await profileCardService.updateProfile(
+      req.body.profileCardId,
+      req.body.newValue,
+      req.body.parentDataKey,
+      req.body.itemIndex
+    )
 
-  if (result) {
-    res.send(true)
-  } else {
-    res.send(false)
+    if (result) {
+      res.send(true)
+    } else {
+      res.send(false)
+    }
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
   }
 })
 
